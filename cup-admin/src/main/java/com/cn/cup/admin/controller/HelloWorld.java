bb785de4954a33fe93474fe00a04e2ce4ee54661
gzl0807@126.com <gzl0807@126.com>
2020-09-16 23:13:19 +0800

    初始化项目

--- cup-admin/src/main/java/com/cn/cup/admin/controller/HelloWorld.java
+++ cup-admin/src/main/java/com/cn/cup/admin/controller/HelloWorld.java
@@ -1,9 +1,8 @@
 package com.cn.cup.admin.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,8 +10,10 @@ import java.util.Map;
 @RestController
 public class HelloWorld {
 
-    @RequestMapping(value = "find", method = RequestMethod.GET)
+
+    @GetMapping(value = "find")
     public Map<String, String> findAll() {
+
         Map<String, String> all = new HashMap<>();
         all.put("remark", "可以返回json，xml或自定义mediaType内容到页面");
         return all;
---
+++ cup-admin/src/test/java/RocketProducerTest.java
@@ -0,0 +1,18 @@
+import com.cn.cup.CupAdminApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {CupAdminApplication.class})
+public class RocketProducerTest {
+
+
+
+    @Test
+    public void test(){
+        System.out.println("HELLO WORLD");
+    }
+}
